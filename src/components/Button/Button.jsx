2a92464e8af2a6b32d49a2321b3e5df9061f8e10
Zaheershaik-SDE-1 <shaikzaheer2002@gmail.com>
2026-01-05 11:20:38 +0530

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <button className='bg-gradient-to-b from-orange-400 to-orange-600 text-white px-8 py-3 rounded-lg hover:scale-105 hover:to-orange-600
+    transition-all duration-300 cursor-pointer '> {props.content}</button>
+  )
+}
+
+export default Button
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar.jsx'
+import Hero from '../Hero/Hero.jsx'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+    </div>
+    
+  )
+}
+
+export default Home
